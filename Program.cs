1f3974b6e4686c61a0017676b0fb82514cf33049
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2022-09-24 18:53:50 +0500

    Ready task1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Lab5
+﻿using System.Globalization;
+
+namespace Lab5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int intNumber;
+            float floatNumber;
+            float oldFloatNumber = 0f;
+            string value;
+
+            do
+            {
+                value = Console.ReadLine();
+
+                if (int.TryParse(value, out intNumber))
+                {
+                    Console.WriteLine((char)intNumber);
+                }
+                else if (float.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out floatNumber))
+                {
+                    if (floatNumber == oldFloatNumber)
+                        break;
+
+                    oldFloatNumber = floatNumber;
+                }
+            } while (value != "q");
         }
     }
 }
