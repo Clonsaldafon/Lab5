9da58e3f8a041061dab2df02d6548a51af72f271
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2022-09-24 19:06:53 +0500

    Checking for a number

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            string number = Console.ReadLine();
+            string number = Console.ReadLine().Replace("-", "");
+
+            if (CheckingNumber(number))
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Not a number!");
+            }
+        }
+
+        static bool CheckingNumber(string n)
+        {
+            bool isNumber = true;
+
+            foreach (char s in n)
+            {
+                if (!char.IsDigit(s))
+                    return false;
+            }
+
+            return isNumber;
         }
     }
 }
