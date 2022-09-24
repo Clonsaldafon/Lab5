a334df3c227168dde239ca1f4c1e942631dddb6e
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2022-09-24 19:44:54 +0500

    Calculating the factorial of positive integers

--- Program.cs
+++ Program.cs
@@ -5,6 +5,34 @@
         static void Main(string[] args)
         {
             string[] numbers = Console.ReadLine().Split();
+            string[] new_array = new string[numbers.Length];
+            Array.Copy(numbers, new_array, numbers.Length);
+
+            int intNumber;
+            double doubleNumber;
+
+            for (int i = 0; i < new_array.Length; i++)
+            {
+                if (int.TryParse(new_array[i], out intNumber))
+                {
+                    if (intNumber > 0)
+                        new_array[i] = Factorial(intNumber);
+                }
+                else if (double.TryParse(new_array[i], out doubleNumber))
+                {
+
+                }
+            }
+        }
+
+        static string Factorial(int n)
+        {
+            int fact = 1;
+
+            for (int i = 2; i <= n; i++)
+                fact *= i;
+
+            return Convert.ToString(fact);
         }
     }
 }
